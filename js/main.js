05633affe4ed9e748716d57713fb5ccf73d60e1f
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-09-30 10:01:44 -0300

    a

---
+++ js/main.js
@@ -0,0 +1,10 @@
+<script>
+        var tituloReal = document.querySelector("h1");
+        console.log(tituloReal);
+        console.log(tituloReal.textContent);
+        tituloReal.textContent = "Outro Nome"
+
+        const meuElemento = document.getElementById("meu-div");
+        meuElemento.innerHTML = "Novo Conteúdo"
+        
+    </script>
