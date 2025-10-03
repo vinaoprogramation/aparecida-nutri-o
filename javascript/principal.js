936c1bd5cb606ecc56a2611068bfd0a2fe9ae166
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-03 10:45:00 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -3,4 +3,5 @@
         console.log(titulo.textContent);
         titulo.textContent = "Outro Nome"
 
-    
+    var paciente = document.querySelector("#primeiro-paciente")
+    console.log(paciente)
