cb3ece115d7a70f47c44ae20dbe9edc8ee4ffd5b
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-03 11:02:39 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,7 +1,26 @@
-        var titulo = document.querySelector("h1");
+    var titulo = document.querySelector("h1");
         console.log(titulo);
         console.log(titulo.textContent);
-        titulo.textContent = "Outro Nome"
+        titulo.textContent = "Outro Nome";
 
     var paciente = document.querySelector("#primeiro-paciente");
-    console.log(paciente);
+    console.log(paciente)
+
+    var peso = paciente.querySelector(".info-peso");
+    console.log(peso)
+
+    var altura = paciente.querySelector(".info-altura");
+    console.log(altura)
+
+    var gordura = paciente.querySelector(".info-gordura");
+    console.log(gordura)
+
+    var calculo-imc = (peso / (altura*altura))
+    console.log(calculo-imc)
+
+    var imc = paciente.querySelector(".info-imc");
+
+        imc.textContent = "Olá"
+
+
+
