de9b20a22826b2ef8590eacc3240d54441d90dac
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-07 08:25:59 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -24,7 +24,115 @@
 
     var imc = tdPaciente.querySelector(".info-imc")
     imc.textContent = calculoImc
-    console.log(imc)
+    
+
+
+
+
+
+    var tdPaciente = document.querySelector("#segundo-paciente");
+    paciente = tdPaciente.textContent
+    console.log(paciente)
+
+    var tdPeso = tdPaciente.querySelector(".info-peso");
+    peso = tdPeso.textContent
+    console.log(peso)
+
+    var tdAltura = tdPaciente.querySelector(".info-altura");
+    altura = tdAltura.textContent
+    console.log(altura)
+
+    var tdGordura = tdPaciente.querySelector(".info-gordura");
+    gordura = tdGordura.textContent
+    console.log(gordura)
+
+    var calculoImc = Math.round(peso / (altura*altura))
+    console.log(calculoImc)
+
+    var imc = tdPaciente.querySelector(".info-imc")
+    imc.textContent = calculoImc
+
+
+
+
+
+
+
+    var tdPaciente = document.querySelector("#terceiro-paciente");
+    paciente = tdPaciente.textContent
+    console.log(paciente)
+
+    var tdPeso = tdPaciente.querySelector(".info-peso");
+    peso = tdPeso.textContent
+    console.log(peso)
+
+    var tdAltura = tdPaciente.querySelector(".info-altura");
+    altura = tdAltura.textContent
+    console.log(altura)
+
+    var tdGordura = tdPaciente.querySelector(".info-gordura");
+    gordura = tdGordura.textContent
+    console.log(gordura)
+
+    var calculoImc = Math.round(peso / (altura*altura))
+    console.log(calculoImc)
+
+    var imc = tdPaciente.querySelector(".info-imc")
+    imc.textContent = calculoImc
+
+
+
+
+
+
+    var tdPaciente = document.querySelector("#quarto-paciente");
+    paciente = tdPaciente.textContent
+    console.log(paciente)
+
+    var tdPeso = tdPaciente.querySelector(".info-peso");
+    peso = tdPeso.textContent
+    console.log(peso)
+
+    var tdAltura = tdPaciente.querySelector(".info-altura");
+    altura = tdAltura.textContent
+    console.log(altura)
+
+    var tdGordura = tdPaciente.querySelector(".info-gordura");
+    gordura = tdGordura.textContent
+    console.log(gordura)
+
+    var calculoImc = Math.round(peso / (altura*altura))
+    console.log(calculoImc)
+
+    var imc = tdPaciente.querySelector(".info-imc")
+    imc.textContent = calculoImc
+
+
+
+
+
+
+    var tdPaciente = document.querySelector("#quinto-paciente");
+    paciente = tdPaciente.textContent
+    console.log(paciente)
+
+    var tdPeso = tdPaciente.querySelector(".info-peso");
+    peso = tdPeso.textContent
+    console.log(peso)
+
+    var tdAltura = tdPaciente.querySelector(".info-altura");
+    altura = tdAltura.textContent
+    console.log(altura)
+
+    var tdGordura = tdPaciente.querySelector(".info-gordura");
+    gordura = tdGordura.textContent
+    console.log(gordura)
+
+    var calculoImc = Math.round(peso / (altura*altura))
+    console.log(calculoImc)
+
+    var imc = tdPaciente.querySelector(".info-imc")
+    imc.textContent = calculoImc
 
 
 
