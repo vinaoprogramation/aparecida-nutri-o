5a6eac19234ba8b5afd7952767e53ab19725a5ac
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-07 09:03:11 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -144,29 +144,3 @@
     imc = parseFloat(imcArredondado)
     imcTexto = tdPaciente.querySelector(".info-imc")
     imcTexto.textContent = imc
-
-
-    function calculoDoImc("#sexto-paciente") {
-        var tdPacienteGlobal = document.querySelector(calculoDoImc);
-        paciente = tdPaciente.textContent
-
-        var tdPesoGlobal = tdPaciente.querySelector(".info-peso");
-        peso = tdPeso.textContent
-
-        var tdAlturaGlobal = tdPaciente.querySelector(".info-altura");
-        altura = tdAltura.textContent
-
-        var tdGorduraGlobal = tdPaciente.querySelector(".info-gordura");
-        gordura = tdGordura.textContent
-
-        var calculoImcGlobal = Math.round(peso / (altura*altura))
-
-        var calculoImcGlobal = (peso / (altura*altura))
-        var imcArredondadoGlobal = calculoImc.toFixed(1)
-        imcGlobal = parseFloat(imcArredondado)
-        imcTextoGlobal = tdPaciente.querySelector(".info-imc")
-        imcTextoGlobal.textContent = imcGlobal
-    }
-
-    calculoDoImc()
-
