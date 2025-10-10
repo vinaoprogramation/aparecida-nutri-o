8b220c87cd3e3de7aed238d81b9636b6004f95cf
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-10 09:37:37 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -15,7 +15,7 @@
     gordura = tdGordura.textContent
     console.log(gordura)
 
-    var calculoImc = (peso / (altura*altura))
+    var calculoImc = int((peso / (altura*altura)))
     var imcArredondado = calculoImc.toFixed(1)
     imc = parseFloat(imcArredondado)
     imcTexto = tdPaciente.querySelector(".info-imc")
@@ -43,7 +43,7 @@
     var calculoImc = Math.round(peso / (altura*altura))
     console.log(calculoImc)
 
-    var calculoImc = (peso / (altura*altura))
+    var calculoImc = int((peso / (altura*altura)))
     var imcArredondado = calculoImc.toFixed(1)
     imc = parseFloat(imcArredondado)
     imcTexto = tdPaciente.querySelector(".info-imc")
@@ -73,7 +73,7 @@
     var calculoImc = Math.round(peso / (altura*altura))
     console.log(calculoImc)
 
-    var calculoImc = (peso / (altura*altura))
+    var calculoImc = int((peso / (altura*altura)))
     var imcArredondado = calculoImc.toFixed(1)
     imc = parseFloat(imcArredondado)
     imcTexto = tdPaciente.querySelector(".info-imc")
@@ -102,7 +102,7 @@
     var calculoImc = Math.round(peso / (altura*altura))
     console.log(calculoImc)
 
-    var calculoImc = (peso / (altura*altura))
+    var calculoImc = int((peso / (altura*altura)))
     var imcArredondado = calculoImc.toFixed(1)
     imc = parseFloat(imcArredondado)
     imcTexto = tdPaciente.querySelector(".info-imc")
@@ -140,3 +140,10 @@
 
 
     
+    if (peso < 0 || peso>1000){
+        console.log("Peso inválido")
+    }
+
+    if (altura < 0 || altura > 3.00){
+        console.log("Altura inválida")
+    }
