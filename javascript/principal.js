10408d6d3581ca68d6f7a91d058c3e8f1dbdeb9d
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-17 09:07:26 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -191,23 +191,6 @@ if (alturaEhValida == true && pesoEhValido == true) {
 
 
 
-var peso_user = document.querySelector("#user_peso")
-var altura = document.querySelector("#user_altura")
-var imc_user = ((peso_user / (altura_user * altura_user)))
-user_data = document.querySelector("#user_imc")
-user_data.textContent = imc_user
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
