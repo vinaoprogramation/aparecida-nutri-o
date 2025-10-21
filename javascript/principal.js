15d0995dca2a3becd093c3daa871b5f7899175e1
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-21 09:14:56 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -7,6 +7,8 @@ console.log(pacientes)
 for (var i = 0; i < pacientes.length ; i++) {
 
     var paciente = pacientes[i]
+
+    paciente.classList.add(".campo-invalido")
     
     var tdPeso = paciente.querySelector(".info-peso");
     peso = tdPeso.textContent
@@ -28,7 +30,7 @@ for (var i = 0; i < pacientes.length ; i++) {
         pesoEhValido = false;
         imcTexto = paciente.querySelector(".info-imc")
         imcTexto.textContent = "Peso Inválido"
-        paciente.classList.add(".campo-invalido")
+        
     }
 
     if (altura < 0 || altura > 3.00) {
