cbfc3821b641f2863202491dc6953da253b38fdb
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-28 09:32:38 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -42,14 +42,14 @@ for (var i = 0; i < pacientes.length ; i++) {
     
     function verifica(peso, altura)
 
-    if (pesoEhValido == false) {
+    if (pesoEhValido) {
         imcTexto = paciente.querySelector(".info-imc")
         imcTexto.textContent = "Peso Inválido"
         paciente.classList.add("campo-invalido")
 
 
     }
-    if (alturaEhValida == false) {
+    if (alturaEhValida) {
         imcTexto = paciente.querySelector(".info-imc")
         imcTexto.textContent = "Altura Inválida"
         paciente.classList.add("campo-invalido")
