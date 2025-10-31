2a2b29120fc2b8881725be8c285922dc30d234c4
Vinícius Silva Xister <vinicius.xister@escola.pr.gov.br>
2025-10-31 10:44:18 -0300

    a

--- javascript/principal.js
+++ javascript/principal.js
@@ -1,5 +1,32 @@
 var pacientes = document.querySelectorAll(".paciente");
 
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+
+    if (peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
 for (var i = 0; i < pacientes.length; i++) {
 
     var paciente = pacientes[i];
@@ -35,30 +62,6 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 }
 
-function calculaImc(peso, altura) {
-    var imc = 0;
-    imc = peso / (altura * altura);
-
-    return imc.toFixed(2);
-}
-
-function validaPeso(peso) {
-
-    if (peso >= 0 && peso <= 1000) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validaAltura(altura) {
-
-    if (altura >= 0 && altura <= 3.00) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 
 
